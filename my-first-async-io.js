0f8462da793f2c8ef67f88cd656102ac7827abce
bulgarellim <bulgarelli.mariana1@gmail.com>
2022-01-17 16:29:58 -0300

    Add source

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -3,12 +3,13 @@
 //  (stdout), similar to running cat file | wc -l.
 // https://stackoverflow.com/questions/10058814/get-data-from-fs-readfile
 // https://www.geeksforgeeks.org/node-js-fs-readfile-method/
+// https://github.com/maxogden/art-of-node#callbacks
 let tamanho = 0;
 const fs = require("fs");
 path = process.argv[2];
 
 function somaLinhas(path) {
-  fs.readFile(path, 'utf8', function doneReading(err, data) {
+  fs.readFile(path, 'utf8', function leitura(err, data) {
     if (err) {
         throw err;
     }
